005d8912fb8fc567296bc2ef3d439c3fc210d395
Patrycja <patrycja_ciosek@hotmail.com>
2023-09-28 18:51:01 +0100

    Added updateBooking test

--- src/test/java/tests/ProductTest.java
+++ src/test/java/tests/ProductTest.java
@@ -41,5 +41,36 @@ public class ProductTest {
         requestFactory.addBooking(requestPayload).then().log().all().statusCode(200);
 
     }
+    @Test
+    public void updateBooking(){
+        String requestPayload = "{\n" +
+                "    \"firstname\" : \"Pati\",\n" +
+                "    \"lastname\" : \"Brown\",\n" +
+                "    \"totalprice\" : 111,\n" +
+                "    \"depositpaid\" : true,\n" +
+                "    \"bookingdates\" : {\n" +
+                "        \"checkin\" : \"2018-01-01\",\n" +
+                "        \"checkout\" : \"2019-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\" : \"Breakfast\"\n" +
+                "}";
+
+        var response = requestFactory.addBooking(requestPayload).then().log().all().statusCode(200);
+        var id = response.extract().path("bookingid").toString();
+        String requestUpdatedPayload = "{\n" +
+                "    \"firstname\" : \"Joe\",\n" +
+                "    \"lastname\" : \"Brown\",\n" +
+                "    \"totalprice\" : 111,\n" +
+                "    \"depositpaid\" : true,\n" +
+                "    \"bookingdates\" : {\n" +
+                "        \"checkin\" : \"2018-01-01\",\n" +
+                "        \"checkout\" : \"2019-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\" : \"Breakfast\"\n" +
+                "}";
+
+        requestFactory.updateBooking(id,requestUpdatedPayload).then().log().all().statusCode(200);
+        requestFactory.getBookingWithIdPage(id).then().log().all();
+    }
 
     }
