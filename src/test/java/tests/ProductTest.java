3875a580d5c944c1c52c7cd3f1b51de61044f28f
Patrycja <patrycja_ciosek@hotmail.com>
2023-09-28 18:42:32 +0100

    implemented addNewBooking Test and extracted id from json response for future use

--- src/test/java/tests/ProductTest.java
+++ src/test/java/tests/ProductTest.java
@@ -28,11 +28,23 @@ public class ProductTest {
     public void goToUserPage(){
         requestFactory.getBookingWithIdPage("87").then().log().all().statusCode(200);
     }
-    @Test @Ignore
+    @Test
     public void addNewBooking(){
-        String requestPayload = "";
+        String requestPayload = "{\n" +
+                "    \"firstname\" : \"Pati\",\n" +
+                "    \"lastname\" : \"Brown\",\n" +
+                "    \"totalprice\" : 111,\n" +
+                "    \"depositpaid\" : true,\n" +
+                "    \"bookingdates\" : {\n" +
+                "        \"checkin\" : \"2018-01-01\",\n" +
+                "        \"checkout\" : \"2019-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\" : \"Breakfast\"\n" +
+                "}";
 
-        requestFactory.addBooking(requestPayload).then().log().all().statusCode(201);
+        var response = requestFactory.addBooking(requestPayload).then().log().all().statusCode(200);
+        var id = response.extract().path("bookingid").toString();
+
     }
 
     }
