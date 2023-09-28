d14627f49d2c5f6f0f3637ed6b6f222569d325b5
Patrycja <patrycja_ciosek@hotmail.com>
2023-09-28 18:49:02 +0100

    Added token authentication into SendPutRequest

--- src/test/java/client/RestClient.java
+++ src/test/java/client/RestClient.java
@@ -36,7 +36,7 @@ public class RestClient {
         return given().contentType(ContentType.JSON).when().body(requestPayload).post(uri);
     }
     public Response SendPutRequest(String uri, Object requestPayload){
-        return given().contentType(ContentType.JSON).when().body(requestPayload).put(uri);
+        return given().header("Cookie", "token="+ access_token).contentType(ContentType.JSON).when().body(requestPayload).put(uri);
     }
     public Response SendPatchRequest(String uri, Object requestPayload){
         return given().contentType(ContentType.JSON).when().body(requestPayload).patch(uri);
