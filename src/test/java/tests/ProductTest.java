e8c834aaeca93897e7b5f2c22a5cb2f061d10b19
Patrycja <patrycja_ciosek@hotmail.com>
2023-09-29 14:07:17 +0100

    Created test for partialUpdate.

--- src/test/java/tests/ProductTest.java
+++ src/test/java/tests/ProductTest.java
@@ -1,8 +1,6 @@
 package tests;
 
-
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import request.RequestFactory;
 
@@ -72,5 +70,28 @@ public class ProductTest {
         requestFactory.updateBooking(id,requestUpdatedPayload).then().log().all().statusCode(200);
         requestFactory.getBookingWithIdPage(id).then().log().all();
     }
+    @Test
+    public void partialUpdate(){
+        String requestPayload = "{\n" +
+                "    \"firstname\" : \"Pati\",\n" +
+                "    \"lastname\" : \"Brown\",\n" +
+                "    \"totalprice\" : 111,\n" +
+                "    \"depositpaid\" : true,\n" +
+                "    \"bookingdates\" : {\n" +
+                "        \"checkin\" : \"2018-01-01\",\n" +
+                "        \"checkout\" : \"2019-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\" : \"Breakfast\"\n" +
+                "}";
+
+        var response = requestFactory.addBooking(requestPayload).then().log().all().statusCode(200);
+        var id = response.extract().path("bookingid").toString();
+
+        String requestUpdate = "{ \"firstname\" : \"James\",\n" +
+                "    \"lastname\" : \"Brown\"}";
+
+        requestFactory.partialUpdateBooking(id,requestUpdate).then().log().all().statusCode(200);
+    }
+
 
     }
